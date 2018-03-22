3a12b01119b45e00e0119b93b676f22d5a048b4c
Myroslav <bmtech@i.ua>
2018-03-22 19:15:43 +0200

    Initial Commit

---
+++ Calculator4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator4
+//
+//  Created by miroslav babilya on 22.03.18.
+//  Copyright © 2018 miroslav babilya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
